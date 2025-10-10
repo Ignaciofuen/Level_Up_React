7c7940ce707efb87e8f954478215a7101d0bfa17
Matias-Riquelme <matia.riquelmea@duocuc.cl>
2025-10-10 16:31:04 -0300

    Add files via upload
    Se agrega pagina de contacto y nosotros

---
+++ Level_Up_React-level_up_react_segundo_avance/src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
+import { AuthProvider } from './auth/AuthContext'
+import Home from './pages/Home'
+import Carrito from './pages/carrito'
+import Productos from './pages/Productos'
+import Contacto from './pages/contacto'
+import Nosotros from './pages/nosotros'
+import AppNavbar from './components/AppNavbar'
+import Footer from './components/footer'
+import './App.css'
+import './styles/custom.css';
+
+
+
+export default function App(){
+const [carrito, setCarrito] = useState([])
+
+
+// Persistir carrito opcionalmente
+useEffect(() => {
+const raw = localStorage.getItem('tg_cart')
+if(raw){ setCarrito(JSON.parse(raw)) }
+}, [])
+
+
+useEffect(() => {
+localStorage.setItem('tg_cart', JSON.stringify(carrito))
+}, [carrito])
+
+
+const addToCart = (item) => setCarrito(prev => [...prev, item])
+const removeFromCart = (index) => setCarrito(prev => prev.filter((_, i) => i !== index))
+const clearCart = () => setCarrito([])
+
+
+
+
+return (
+<AuthProvider>
+<Router>
+<AppNavbar  cartItems={carrito} />
+<Routes>
+<Route path="/" element={<Home/>} />
+<Route path="/Productos" element={<Productos onAdd={addToCart} />} />
+<Route path="/carrito" element={
+/*<ProtectedRoute>*/
+<Carrito items={carrito} onRemove={removeFromCart} onClear={clearCart} />
+/*</ProtectedRoute>*/
+} />
+<Route path="/contacto" element={<Contacto />} />
+<Route path="/nosotros" element={<Nosotros />} />
+</Routes>
+<Footer/>
+</Router>
+</AuthProvider> 
+)
+
+}
